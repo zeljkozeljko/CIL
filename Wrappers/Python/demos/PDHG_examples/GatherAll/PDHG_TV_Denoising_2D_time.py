be5070ad14d46b1cd93d76eda80b06737dddae36
epapoutsellis <epapoutsellis@gmail.com>
2019-06-14 16:39:09 +0100

    fix 2D Den time tomo

--- Wrappers/Python/demos/PDHG_examples/GatherAll/PDHG_TV_Denoising_2D_time.py
+++ Wrappers/Python/demos/PDHG_examples/GatherAll/PDHG_TV_Denoising_2D_time.py
@@ -50,9 +50,9 @@ from ccpi.optimisation.functions import ZeroFunction, L2NormSquared,  \
 import os
 import tomophantom
 from tomophantom import TomoP2D
+import matplotlib.animation as animation
 
 # Create phantom for TV 2D dynamic tomography 
-
 model = 102  # note that the selected model is temporal (2D + time)
 N = 64 # set dimension of the phantom
 
@@ -60,15 +60,15 @@ path = os.path.dirname(tomophantom.__file__)
 path_library2D = os.path.join(path, "Phantom2DLibrary.dat")
 phantom_2Dt = TomoP2D.ModelTemporal(model, N, path_library2D)
 
-plt.close('all')
-plt.figure(1)
-plt.rcParams.update({'font.size': 21})
-plt.title('{}''{}'.format('2D+t phantom using model no.',model))
-for sl in range(0,np.shape(phantom_2Dt)[0]):
-    im = phantom_2Dt[sl,:,:]
-    plt.imshow(im, vmin=0, vmax=1)
-#    plt.pause(.1)
-#    plt.draw
+# Animate
+fig = plt.figure()
+ims1 = []
+for sl in range(0,np.shape(phantom_2Dt)[0]):    
+    im1 = plt.imshow(phantom_2Dt[sl,:,:], animated=True, vmin=0, vmax=1)    
+    ims1.append([im1])
+ani1 = animation.ArtistAnimation(fig, ims1, interval=500, blit = True,
+                                repeat_delay=10)
+plt.show() 
 
 # Setup geometries    
 ig = ImageGeometry(voxel_num_x = N, voxel_num_y = N, channels = np.shape(phantom_2Dt)[0])
@@ -82,7 +82,7 @@ noisy_data = ImageData( data.as_array() + np.random.normal(0, 0.25, size=ig.shap
 # time-frames index
 tindex = [8, 16, 24]
 
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 10))
+fig1, axes1 = plt.subplots(nrows=1, ncols=3, figsize=(10, 10))
 plt.subplot(1,3,1)
 plt.imshow(noisy_data.as_array()[tindex[0],:,:])
 plt.axis('off')
@@ -96,36 +96,34 @@ plt.imshow(noisy_data.as_array()[tindex[2],:,:])
 plt.axis('off')
 plt.title('Time {}'.format(tindex[2]))
 
-fig.subplots_adjust(bottom=0.1, top=0.9, left=0.1, right=0.8,
+fig1.subplots_adjust(bottom=0.1, top=0.9, left=0.1, right=0.8,
                     wspace=0.02, hspace=0.02)
-
+plt.tight_layout()
 plt.show()   
 
 # Regularisation Parameter
 alpha = 0.3
 
-# Create operators
-op1 = Gradient(ig, correlation='Space')
-op2 = Gradient(ig, correlation='SpaceChannels')
+# Create Gradient operators with different Space - Channel correlation
+op1 = Gradient(ig, correlation='Space') # No gradient in temporal direction
+op2 = Gradient(ig, correlation='SpaceChannels') # SpatioTemporal Gradient
 op3 = Identity(ig, ag)
 
 # Create BlockOperator
 operator1 = BlockOperator(op1, op3, shape=(2,1) ) 
 operator2 = BlockOperator(op2, op3, shape=(2,1) )
 
-# Create functions
-      
+# Create functions     
 f1 = alpha * MixedL21Norm()
 f2 = 0.5 * L2NormSquared(b = noisy_data)    
-f = BlockFunction(f1, f2)  
-                                                                        
+f = BlockFunction(f1, f2)                                                                          
 g = ZeroFunction()
     
 # Compute operator Norm
 normK1 = operator1.norm()
 normK2 = operator2.norm()
 
-#%%
+
 # Primal & dual stepsizes
 sigma1 = 1
 tau1 = 1/(sigma1*normK1**2)
@@ -137,99 +135,99 @@ tau2 = 1/(sigma2*normK2**2)
 pdhg1 = PDHG(f=f,g=g,operator=operator1, tau=tau1, sigma=sigma1)
 pdhg1.max_iteration = 2000
 pdhg1.update_objective_interval = 200
-pdhg1.run(2000)
+pdhg1.run(20)
 
 # Setup and run the PDHG algorithm
 pdhg2 = PDHG(f=f,g=g,operator=operator2, tau=tau2, sigma=sigma2)
 pdhg2.max_iteration = 2000
 pdhg2.update_objective_interval = 200
-pdhg2.run(2000)
-
+pdhg2.run(20)
 
 #%%
-
 tindex = [8, 16, 24]
-fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 8))
+fig2, axes2 = plt.subplots(nrows=3, ncols=3, figsize=(10, 10))
 
-plt.subplot(3,3,1)
-plt.imshow(phantom_2Dt[tindex[0],:,:])
-plt.axis('off')
-plt.title('Time {}'.format(tindex[0]))
-
-plt.subplot(3,3,2)
-plt.imshow(phantom_2Dt[tindex[1],:,:])
-plt.axis('off')
-plt.title('Time {}'.format(tindex[1]))
-
-plt.subplot(3,3,3)
-plt.imshow(phantom_2Dt[tindex[2],:,:])
-plt.axis('off')
-plt.title('Time {}'.format(tindex[2]))
-
-plt.subplot(3,3,4)
-plt.imshow(pdhg1.get_output().as_array()[tindex[0],:,:])
-plt.axis('off')
-plt.subplot(3,3,5)
-plt.imshow(pdhg1.get_output().as_array()[tindex[1],:,:])
-plt.axis('off')
-plt.subplot(3,3,6)
-plt.imshow(pdhg1.get_output().as_array()[tindex[2],:,:])
-plt.axis('off')
+# Ground Truth
+axes2[0, 0].imshow(phantom_2Dt[tindex[0],:,:])
+axes2[0, 0].set_title('Time {}'.format(tindex[0]))
+axes2[0,0].set_ylabel('Ground Truth')
 
 
-plt.subplot(3,3,7)
-plt.imshow(pdhg2.get_output().as_array()[tindex[0],:,:])
-plt.axis('off')
-plt.subplot(3,3,8)
-plt.imshow(pdhg2.get_output().as_array()[tindex[1],:,:])
-plt.axis('off')
-plt.subplot(3,3,9)
-plt.imshow(pdhg2.get_output().as_array()[tindex[2],:,:])
-plt.axis('off')
+axes2[0, 1].imshow(phantom_2Dt[tindex[1],:,:])
+axes2[0, 1].set_title('Time {}'.format(tindex[1]))
+axes2[0,1].axis('off')
+
+axes2[0, 2].imshow(phantom_2Dt[tindex[2],:,:])
+axes2[0, 2].set_title('Time {}'.format(tindex[2]))
+axes2[0,2].axis('off')
+
+# Space Correlation 
+axes2[1, 0].imshow(pdhg1.get_output().as_array()[tindex[0],:,:], vmin=0, vmax =1)
+axes2[1,0].set_ylabel('Space \n Correlation')
+
+axes2[1, 1].imshow(pdhg1.get_output().as_array()[tindex[1],:,:])
+axes2[1,1].axis('off')
+axes2[1, 2].imshow(pdhg1.get_output().as_array()[tindex[2],:,:])
+axes2[1,2].axis('off')
+
+# Space Channel Correlation 
+axes2[2, 0].imshow(pdhg2.get_output().as_array()[tindex[0],:,:])
+axes2[2,0].set_ylabel('SpaceTime \n Correlation')
+
+axes2[2, 1].imshow(pdhg2.get_output().as_array()[tindex[1],:,:])
+axes2[2,1].axis('off')
+
+axes2[2, 2].imshow(pdhg2.get_output().as_array()[tindex[2],:,:])
+axes2[2,2].axis('off')
 
 
-im = plt.imshow(pdhg1.get_output().as_array()[tindex[0],:,:])
-
-
-fig.subplots_adjust(bottom=0.1, top=0.9, left=0.1, right=0.8,
+fig2.subplots_adjust(bottom=0.1, top=0.9, left=0.1, right=0.8,
                     wspace=0.02, hspace=0.02)
 
-cb_ax = fig.add_axes([0.83, 0.1, 0.02, 0.8])
-cbar = fig.colorbar(im, cax=cb_ax)
+plt.tight_layout()
 plt.show()
 
 #%%
-import matplotlib.animation as animation
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 30))
+# Animate
+fig3 = plt.figure()
+
+sol1 = pdhg1.get_output().as_array()
+sol2 = pdhg2.get_output().as_array()
+
 ims1 = []
 ims2 = []
 ims3 = []
+
 for sl in range(0,np.shape(phantom_2Dt)[0]):
     
     plt.subplot(1,3,1)
     im1 = plt.imshow(phantom_2Dt[sl,:,:], animated=True)
+    plt.title('Ground truth')
     
     plt.subplot(1,3,2)
-    im2 = plt.imshow(pdhg1.get_output().as_array()[sl,:,:])
+    im2 = plt.imshow(sol1[sl,:,:], animated=True) 
+    plt.title('Space \n Correlation')
     
     plt.subplot(1,3,3)
-    im3 = plt.imshow(pdhg2.get_output().as_array()[sl,:,:])
+    im3 = plt.imshow(sol2[sl,:,:], animated=True)  
+    plt.title('Space-Time \n Correlation')
     
     ims1.append([im1])
-    ims2.append([im2])
-    ims3.append([im3])
+    ims2.append([im2])   
+    ims3.append([im3])       
+
     
     
-ani1 = animation.ArtistAnimation(fig, ims1, interval=500,
+ani1 = animation.ArtistAnimation(fig3, ims1, interval=500,
                                 repeat_delay=10)
 
-ani2 = animation.ArtistAnimation(fig, ims2, interval=500,
+ani2 = animation.ArtistAnimation(fig3, ims2, interval=500,
                                 repeat_delay=10)
 
-ani3 = animation.ArtistAnimation(fig, ims3, interval=500,
+ani3 = animation.ArtistAnimation(fig3, ims3, interval=500,
                                 repeat_delay=10)
-plt.show()        
 
+plt.show() 
 
 
 
