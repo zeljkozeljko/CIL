7e336331eb1550402244b3c5a140a15d48407e9e
epapoutsellis <epapoutsellis@gmail.com>
2022-03-06 16:37:07 +0000

    add logging info setup PDHG

--- Wrappers/Python/cil/optimisation/algorithms/PDHG.py
+++ Wrappers/Python/cil/optimisation/algorithms/PDHG.py
@@ -20,7 +20,7 @@ from cil.optimisation.algorithms import Algorithm
 import warnings
 import numpy as np
 from numbers import Number
-
+import logging
 
 
 class PDHG(Algorithm):
@@ -333,7 +333,7 @@ class PDHG(Algorithm):
             Initial point for the PDHG algorithm.
         theta : Relaxation parameter, Number, default 1.0
         """
-        print("{} setting up".format(self.__class__.__name__, ))
+        logging.info("{} setting up".format(self.__class__.__name__, ))
         
         # Triplet (f, g, K)
         self.f = f
@@ -364,8 +364,8 @@ class PDHG(Algorithm):
         if self.gamma_fconj is not None:
             warnings.warn("Dual Acceleration of PDHG: The convex conjugate of function f is assumed to be strongly convex with positive parameter `gamma_fconj`. You need to be sure that gamma_fconj = {} is the correct strongly convex constant".format(self.gamma_fconj))
         
-        self.configured = True
-        print("{} configured".format(self.__class__.__name__, ))
+        self.configured = True     
+        logging.info("{} configured".format(self.__class__.__name__, ))
 
 
     def update_previous_solution(self):
